178f7610a619bdc5944b940879d485fb0bd398c2
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-01-07 18:57:53 +0100

    load optimization

--- purgecss.config.js
+++
@@ -1,7 +0,0 @@
-// purgecss.config.js
-module.exports = {
-    content: ['./**/*.html', './**/*.js'], // Specify the files where PurgeCSS should look for classes
-    css: ['./**/*.css'], // Specify the CSS files to be purged
-    // Other configuration options go here
-  };
-  
