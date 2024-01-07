616b18f8de79d572044a45021ab600b9fecc27bf
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-01-07 18:16:00 +0100

    aria-labels for buttons

---
+++ purgecss.config.js
@@ -0,0 +1,7 @@
+// purgecss.config.js
+module.exports = {
+    content: ['./**/*.html', './**/*.js'], // Specify the files where PurgeCSS should look for classes
+    css: ['./**/*.css'], // Specify the CSS files to be purged
+    // Other configuration options go here
+  };
+  
