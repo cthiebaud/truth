2a42a0ae0a0e67ea62d045fa41a304409a98b96d
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-02-02 09:23:28 +0100

    commented

--- fetch_then_purge_then_copy2dist_bootstrap_css.sh
+++ fetch_then_purge_then_copy2dist_bootstrap_css.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-echo "fetching then purging the copying bootstrap.css to dist"
+echo "fetching then purging then copying bootstrap.css to dist"
 rm -f dist/css/bootstrap.min.css
-purgecss --css <(curl -k https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css -o -) --content index.html --output dist/css/bootstrap.min.css
+purgecss --css <(curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css -o -) --content index.html --output dist/css/bootstrap.min.css
