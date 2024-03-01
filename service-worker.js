1723b73c095fde45e029acb0cf4ff1d714f5443a
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-01 22:11:41 +0100

    from heavy doIt to lighter scrambleIt

--- service-worker.js
+++ service-worker.js
@@ -53,7 +53,7 @@ if (workbox) {
     { revision: 'd6b4d8fe98711da93309de111ab0cb0b', url: 'HOWTO.md' },
     { revision: '318f1210aabf904c588a8cbb2b30c571', url: 'icon.png' },
     { revision: '5ca35952e03f6b381dc92b93a4d1430a', url: 'icon2.png' },
-    { revision: '3a3288a2b59ac98a4fc1f0068ea24992', url: 'index.html' },
+    { revision: 'd936a3b4e1d315fd6cbccba847b3c5ea', url: 'index.html' },
     { revision: '914dd9efa90f70224790ea5710dc31f3', url: 'README.md' },
     { revision: '116f32b33817a41a6dea393a7e96b96c', url: 'screenshots/1024x500-screenshot.png' },
     { revision: '4628442658184696eb42308be406a20d', url: 'screenshots/1280x800-screenshot.png' },
