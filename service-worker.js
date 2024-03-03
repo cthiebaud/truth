2c48921b9b601885db37997707405edf6a6ad057
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-03 11:50:45 +0100

    pffff

--- service-worker.js
+++ service-worker.js
@@ -52,7 +52,7 @@ if (workbox) {
     { revision: 'd6b4d8fe98711da93309de111ab0cb0b', url: 'HOWTO.md' },
     { revision: '318f1210aabf904c588a8cbb2b30c571', url: 'icon.png' },
     { revision: '5ca35952e03f6b381dc92b93a4d1430a', url: 'icon2.png' },
-    { revision: 'b1b8b42a8de1ab1ffdcd747395ca3681', url: 'index.html' },
+    { revision: '3adb3f1046efc6f6da808e305c9a96e5', url: 'index.html' },
     { revision: '914dd9efa90f70224790ea5710dc31f3', url: 'README.md' },
     { revision: '116f32b33817a41a6dea393a7e96b96c', url: 'screenshots/1024x500-screenshot.png' },
     { revision: '4628442658184696eb42308be406a20d', url: 'screenshots/1280x800-screenshot.png' },
