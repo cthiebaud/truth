342bfb74f31188dab5d826e7ea246da32691de31
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-05 15:30:40 +0100

    tweak gradientize algorithm

--- video/ffmpeg.sh
+++ video/ffmpeg.sh
@@ -1,3 +1,6 @@
 ffmpeg -i input.mov -vcodec libx265 -crf 28 output.mp4
 
-ffmpeg -i input.mov -vf "scale=trunc(iw/4)*2:trunc(ih/4)*2" -c:v libx264 -crf 28 output2.mp4
+ffmpeg -i 2024-03-05_14.29.12.mov -vf "scale=trunc(iw/4)*2:trunc(ih/4)*2" -c:v libx264 -crf 28 output2.mp4
+
+
+2024-03-05_14.29.12.mov
