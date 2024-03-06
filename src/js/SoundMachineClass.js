7eb099057d70caab9ac2f1386db90ca12271fb45
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-06 10:09:13 +0100

    new sound for warrior (achilles) level

--- src/js/SoundMachineClass.js
+++ src/js/SoundMachineClass.js
@@ -52,6 +52,7 @@ export class SoundMachine {
         this.boredom = SoundMachine.newHowl({ src: ['/assets/audio/boredom.mp3'] });
         this.abort = SoundMachine.newHowl({ src: ['/assets/audio/short-whoosh.mp3'] });
         this.riser = SoundMachine.newHowl({ src: ['/assets/audio/scary-riser.mp3'], loop: false, volume: 0.2 });
+        this.warrior = SoundMachine.newHowl({ src: ['/assets/audio/warrior.mp3'], loop: false, volume: 1 });
     }
 
 }
