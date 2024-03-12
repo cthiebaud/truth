43d6f3c2ac996f5ef5228d5c9e8a9b4362b67a2a
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-12 23:42:08 +0100

    reorg

--- workbox-config.js
+++ workbox-config.js
@@ -1,18 +1,22 @@
 module.exports = {
   globDirectory: './',
   globPatterns: [
-    '**/*.{html,md,ico,png,svg}',
+    '**/*.{html,md,ico,png,jpg,jpeg,svg,mp3}',
     'dist/**/*.{css,js}'
   ],
   globIgnores: [
+    "*.js",
     "**/node_modules/**/*",
+    "assets/audio/sources/**",
+    "audio-samples/**",
+    "deprecated/**",
+    "packages/**",
+    "screenshots/**",
     "service-worker-src.js",
     "service-worker.js",
-    "assets/audio/sources/**",
-    "test/**",
-    "packages/**",
-    "*.js",
     "svg/**",
+    "test/**",
+    "video/**",
   ],
   swSrc: 'service-worker-src.js',
   swDest: 'service-worker.js',
