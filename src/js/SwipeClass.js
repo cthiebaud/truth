4978a129ed6fad650f3a6ce7563e1b0e2dbf95eb
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-21 09:10:09 +0100

    decent manual swipe for opening/closing info tab

--- src/js/SwipeClass.js
+++ src/js/SwipeClass.js
@@ -1,16 +1,27 @@
 // SwipeClass.js
 
-class Swipe {
-    constructor(elementId) {
+export class Swipe {
+    constructor(elementId, swipeHandler) {
         this.swipeArea = document.getElementById(elementId);
+
+        if (!this.swipeArea) {
+            throw new Error(`Element with id '${elementId}' not found.`);
+        }
 
         this.startX = 0;
         this.currentX = 0;
+        this.swipeHandler = swipeHandler;
 
         // Set touch event listeners as non-passive
         this.swipeArea.addEventListener('touchstart', this.handleTouchStart.bind(this), { passive: false });
         this.swipeArea.addEventListener('touchmove', this.handleTouchMove.bind(this), { passive: false });
         this.swipeArea.addEventListener('touchend', this.handleTouchEnd.bind(this), { passive: false });
+
+        // Store a reference to the element for swipe events
+        this.swipeElement = this.swipeArea;
+
+        // Listen for swipe events on the swipe element
+        this.swipeElement.addEventListener('swipe', this.handleSwipe.bind(this));
     }
 
     handleTouchStart(event) {
@@ -35,8 +46,13 @@ class Swipe {
 
     fireSwipeEvent(direction) {
         const swipeEvent = new CustomEvent('swipe', { detail: direction });
-        this.swipeArea.dispatchEvent(swipeEvent);
+        this.swipeElement.dispatchEvent(swipeEvent);
+    }
+
+    handleSwipe(event) {
+        // Invoke the swipe handler callback provided by the client
+        if (typeof this.swipeHandler === 'function') {
+            this.swipeHandler(event.detail);
+        }
     }
 }
-
-export default Swipe;
