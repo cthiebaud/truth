3de106876ea80626f99bbd54347e453434878de0
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-28 07:04:45 +0100

    truly purged bootstrap

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,13 +2,22 @@ module.exports = {
     safelist: {
         greedy: [
             /modal-backdrop/,
+            /offcanvas$/,
+            /caption-top/,
+            /show$/,
+            /table-borderless$/,
             /carousel$/,
+            /^carousel/,
+        ],
+    },
+}
+
+/*
             /carousel-item$/,
             /carousel-item-next$/,
             /carousel-item-prev$/,
             /carousel-item-end$/,
             /carousel-item-start$/,
             /carousel-control-prev$/,
-            /carousel-control-next$/,],
-    },
-}
+            /carousel-control-next$/,
+*/
--- scripts/fetch_then_purge_then_copy2dist_bootstrap_css.sh
+++ scripts/fetch_then_purge_then_copy2dist_bootstrap_css.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 echo "fetching then purging then copying bootstrap.min.css to dist/css"
 rm -f dist/css/bootstrap.min.css
-purgecss --css <(curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css -o -) --content index.html --config purgecss.config.js --output dist/css/bootstrap.min.css
+rm -f src/css/bootstrap.css
+# purgecss --css <(curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css -o -) --content index.html --config purgecss.config.js --output dist/css/bootstrap.min.css
 # curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.css -o src/css/bootstrap.css
+purgecss --css <(curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.css -o -) --content index.html --config purgecss.config.js --output src/css/bootstrap.css
