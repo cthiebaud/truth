69ad714ba3c137fe4fbbcc0f28d8ffa4e524eff3
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-28 06:38:34 +0100

    small bootstrap ...

--- scripts/fetch_then_purge_then_copy2dist_bootstrap_css.sh
+++ scripts/fetch_then_purge_then_copy2dist_bootstrap_css.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-echo "fetching then purging then copying bootstrap.css to dist"
+echo "fetching then purging then copying bootstrap.min.css to dist/css"
 rm -f dist/css/bootstrap.min.css
 purgecss --css <(curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css -o -) --content index.html --config purgecss.config.js --output dist/css/bootstrap.min.css
 # curl -k https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.css -o src/css/bootstrap.css
